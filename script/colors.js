2c6aa81638d9033ee427f43d9963ca89cb91360e
Uneal Dungo <114863816+yunil30@users.noreply.github.com>
2022-10-15 10:09:38 +0800

    Add files via upload

---
+++ script/colors.js
@@ -0,0 +1,31 @@
+//Functions used in the onclick of the buttons.
+function colorCharcoal(){
+    document.body.style.backgroundColor = '#264653';
+    var titleSection = document.getElementById("title");
+    titleSection.style.color = 'white';
+}
+
+function colorPersianGreen(){
+    document.body.style.backgroundColor = '#2A9D8F';
+    var titleSection = document.getElementById("title");
+    titleSection.style.color = 'white';
+}
+
+function colorMaizeCrayola(){
+    document.body.style.backgroundColor = '#E9C46A';
+    var titleSection = document.getElementById("title");
+    titleSection.style.color = 'white';
+}
+
+function colorSandyBrown(){
+    document.body.style.backgroundColor = '#F4A261';
+    var titleSection = document.getElementById("title");
+    titleSection.style.color = 'white';
+}
+
+function colorBurntSienna(){
+    document.body.style.backgroundColor = '#E76F51';
+    var titleSection = document.getElementById("title");
+    titleSection.style.color = 'white';
+}
+
