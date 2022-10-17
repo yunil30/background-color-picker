e1faabfcb6cbfb3170766f5cbae846ff7506f401
Uneal Dungo <114863816+yunil30@users.noreply.github.com>
2022-10-17 18:17:10 +0800

    Add files via upload

--- script/colors.js
+++ script/colors.js
@@ -1,31 +1,47 @@
+//Color of the buttons.
+var btnCharcoal = document.getElementById('colorCharcoal');
+btnCharcoal.style.backgroundColor = '#264653';
+
+var btnPersianGreen = document.getElementById('colorPersianGreen');
+btnPersianGreen.style.backgroundColor = '#2A9D8F';
+
+var btnMaizeCrayola = document.getElementById('colorMaizeCrayola');
+btnMaizeCrayola.style.backgroundColor = '#E9C46A';
+
+var btnSandyBrown = document.getElementById('colorSandyBrown');
+btnSandyBrown.style.backgroundColor = '#F4A261';
+
+var btnBurntSienna = document.getElementById('colorBurntSienna');
+btnBurntSienna.style.backgroundColor = '#E76F51';
+
 //Functions used in the onclick of the buttons.
 function colorCharcoal(){
     document.body.style.backgroundColor = '#264653';
     var titleSection = document.getElementById("title");
     titleSection.style.color = 'white';
 }
-
 function colorPersianGreen(){
     document.body.style.backgroundColor = '#2A9D8F';
     var titleSection = document.getElementById("title");
     titleSection.style.color = 'white';
 }
-
 function colorMaizeCrayola(){
     document.body.style.backgroundColor = '#E9C46A';
     var titleSection = document.getElementById("title");
     titleSection.style.color = 'white';
 }
-
 function colorSandyBrown(){
     document.body.style.backgroundColor = '#F4A261';
     var titleSection = document.getElementById("title");
     titleSection.style.color = 'white';
 }
-
 function colorBurntSienna(){
     document.body.style.backgroundColor = '#E76F51';
     var titleSection = document.getElementById("title");
     titleSection.style.color = 'white';
 }
+
+
+
+
 
